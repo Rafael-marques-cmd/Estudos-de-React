de131068f71f236dc6259f943eb013a539f4d7ed
Rafael-marques-cmd <marquesrafael1243@gmail.com>
2026-01-14 15:30:08 -0300

    Aprendendo sobre evendos com funções e renderização com react

--- 2-FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2-FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 
 const Events = () => {
+    const HandleClick = (e) => {
+        console.log(e);
+        console.log("Executou");
+    };
+
+    // 8 - função de renderização
+    const RenderSomething = (x) => {
+        if(x) {
+            return <h1>Renderizando isso!</h1>
+        }else {
+            return <h1>Renderizando outra coisa!</h1>
+        }
+    };
+   // return 10 > 2 && <p>Carregando...</p>;
+    
   return (
     <div>
         <div>
             <button onClick={() => console.log("Testando um evento")}>Clique aqui</button>
         </div>
+        {/* 7 - evento com função */}
+        <div>
+            <button onClick={HandleClick}>Clique aqui - com função</button>
+        </div>
+        {/* 8 - Função com render */}
+        {RenderSomething(true)}
+        {RenderSomething(false)}
     </div>
-  )
-}
+  );
+};
 
 export default Events
