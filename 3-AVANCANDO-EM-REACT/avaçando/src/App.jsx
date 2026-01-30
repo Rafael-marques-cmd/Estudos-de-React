578a7c39f81063cdd539d4dc5aab0df81a0ae200
Rafael-marques-cmd <marquesrafael1243@gmail.com>
2026-01-30 16:40:55 -0300

    Adiciona novos componentes e funcionalidades: ChangeMessage, Container, ExecuteFunction, Frafment e Message; atualiza package.json e package-lock.json com informações adicionais.

--- 3-AVANCANDO-EM-REACT/avaçando/src/App.jsx
+++ 3-AVANCANDO-EM-REACT/avaçando/src/App.jsx
@@ -18,7 +18,43 @@ import ShowUserName from './components/ShowUserName';
 // 9 - desestruturando props
 import CarDetals from './components/CarDetals';
 
+// 11 - renderização de listas com componente
+const cars = [
+  {id: 1, brand: "Ferrari", color: "Amarelo", km: 0 },
+  {id: 2, brand: "KIA", color: "Preto", km: 23261 },
+  {id: 3, brand: "Rennault", color: "Azul", km: 45419 },
+];
+
+// 12 - fragments
+import Frafment from './components/Frafment';
+
+// 13 - children
+import Container from './components/Container';
+
+// 14 - funcao em prop
+import ExecuteFunction from './components/ExecuteFunction';
+
+// 15 - state lift
+import { useState } from 'react';
+import Message from './components/Message';
+import ChangeMessage from './components/ChangeMessage';
+
+
+
 function App() {
+  // 14 - função em prop
+  function showMessage() {
+    console.log("Evento do componente pai");
+  }
+
+// 15 - state lift
+const [message, setMessage] = useState("");
+
+const handleMessage = (msg) => {
+  setMessage(msg);
+};
+
+
   return <div className="App" style={{paddingBottom: "500px"}}>
     <h1>Avançando em React</h1>
     {/* 1 - imagem em public */}
@@ -35,7 +71,32 @@ function App() {
     <ShowUserName name="João"/>
     {/* 9 - desestruturando props */}
     <CarDetals brand="VW" km={999} color="Vermelho" />
+    {/* 10 - reaproveitamento de componentes */}
+    <CarDetals brand="Fiat" km={251619} color="Branco" />
+    <CarDetals km={1236} color="Verde" brand="Audi" />
+    {/* 11 - renderização de lista com componente */}
+    {cars.map((car) => (
+      <CarDetals 
+      key={car.id} 
+      brand={car.brand} 
+      color={car.color} 
+      km={car.km}/>
+    ))}
+    {/* 12 - fragments */}
+    <Frafment />
+    {/* 13 - children */}
+    <Container>
+      <div>
+        <h1>Teste</h1>
+        <p>Meu container</p>
+      </div>
+    </Container>
+    {/* 14 - função em prop */}
+    <ExecuteFunction myFunction={showMessage}/>
+    {/* 15 - state lift */}
+    <Message msg={message} />
+    <ChangeMessage handleMessage={handleMessage}/>
   </div>
 }   
 
-export default App
+export default App;
