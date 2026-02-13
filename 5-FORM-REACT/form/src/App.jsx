a8e31f0c6eb058c55252c59469ba759e8edaf4e8
Rafael-marques-cmd <marquesrafael1243@gmail.com>
2026-02-13 10:58:38 -0300

    fazendo um form com react

---
+++ 5-FORM-REACT/form/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MyForm from './components/MyForm';
+function App() {
+
+
+  return (
+    <div>
+      <h1>Form em React</h1>
+      <MyForm 
+      userName="Rafael" 
+      userEmail="platinaaa@gmail.com"/>
+    </div>
+  )
+}
+export default App;
